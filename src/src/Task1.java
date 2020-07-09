e081df8c5382b580226e5f023afbef14f0838c38
Vladislav Chichuk <jake0_0@mail.ru>
2020-07-09 20:06:21 +0300

    add test class

--- src/src/Task1.java
+++ src/src/Task1.java
@@ -1,7 +1,8 @@
 public class Task1 {
 
     public static void main(String[] args) {
-        //System.out.println("String") ;
+        System.out.println("String") ;
+        int a = 20 ;
 
     }
 
